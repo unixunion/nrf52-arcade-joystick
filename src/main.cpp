486c063d140317ebf8cee03ee0b13be317d16418
Kegan Holtzhausen <marzubus@gmail.com>
2020-10-12 15:30:40 +0200

    comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,5 @@
-/*********************************************************************
- This is an example for our nRF52 based Bluefruit LE modules
- Pick one up today in the adafruit shop!
- Adafruit invests time and resources providing this open source code,
- please support Adafruit and open-source hardware by purchasing
- products from Adafruit!
- MIT license, check LICENSE for more information
- All text above, and the splash screen below must be included in
- any redistribution
-*********************************************************************/
-
-/* This sketch demonstate how to use BLE HID to scan an array of pin
- * and send its keycode. It is essentially an implementation of hid keyboard,
- * useful reference if you want to make an BLE keyboard.
+/* 
+This is a simple bluetooth joystic control using nrf52.
  */
 
 #include <Arduino.h>
@@ -21,15 +9,9 @@ BLEDis bledis;
 BLEHidAdafruit blehid;
 
 // Array of pins and its keycode
-// For keycode definition see BLEHidGeneric.h
 uint8_t pins[]    = { 11, A0, A1, A2, A3, A4, A5, 7, 15, 16, 31, 30};
 uint8_t hidcode[] = { HID_KEY_SHIFT_LEFT, HID_KEY_ARROW_UP, HID_KEY_ARROW_DOWN, HID_KEY_ARROW_LEFT ,HID_KEY_ARROW_RIGHT, HID_KEY_Q, HID_KEY_W, HID_KEY_RETURN, HID_KEY_Z, HID_KEY_X, HID_KEY_A, HID_KEY_S };
-
 uint8_t pincount = sizeof(pins)/sizeof(pins[0]);
-
-// Modifier keys, only take cares of Shift
-// ATL, CTRL, CMD keys are left for user excersie.
-//uint8_t shiftPin = 11;
 
 bool keyPressedPreviously = false;
 
@@ -72,15 +54,7 @@ void startAdv(void)
   // There is enough room for the dev name in the advertising packet
   Bluefruit.Advertising.addName();
   
-  /* Start Advertising
-   * - Enable auto advertising if disconnected
-   * - Interval:  fast mode = 20 ms, slow mode = 152.5 ms
-   * - Timeout for fast mode is 30 seconds
-   * - Start(timeout) with timeout = 0 will advertise forever (until connected)
-   * 
-   * For recommended advertising interval
-   * https://developer.apple.com/library/content/qa/qa1931/_index.html   
-   */
+  // Start Advertising 
   Bluefruit.Advertising.restartOnDisconnect(true);
   Bluefruit.Advertising.setInterval(32, 244);    // in unit of 0.625 ms
   Bluefruit.Advertising.setFastTimeout(30);      // number of seconds in fast mode
@@ -88,8 +62,6 @@ void startAdv(void)
   Bluefruit.Periph.setDisconnectCallback(disconnect_callback);
   Bluefruit.Advertising.start(0);                // 0 = Don't stop advertising after n seconds
 }
-
-
 
 
 
@@ -107,13 +79,6 @@ void flash_connected_led()
 }
 
 
-/**
- * Callback invoked when received Set LED from central.
- * Must be set previously with setKeyboardLedCallback()
- *
- * The LED bit map is as follows: (also defined by KEYBOARD_LED_* )
- *    Kana (4) | Compose (3) | ScrollLock (2) | CapsLock (1) | Numlock (0)
- */
 void set_keyboard_led(uint16_t conn_handle, uint8_t led_bitmap)
 {
   (void) conn_handle;
@@ -135,16 +100,6 @@ void setup()
   while ( !Serial ) delay(10);   // for nrf52840 with native usb
 
   Serial.println("Bluefruit52 HID Keyscan Example");
-  // Serial.println("-------------------------------\n");
-
-  // Serial.println();
-  // Serial.println("Go to your phone's Bluetooth settings to pair your device");
-  // Serial.println("then open an application that accepts keyboard input");
-
-  // Serial.println();
-  // Serial.println("Wire configured Pin to GND to send key");
-  // Serial.println("Wire Shift Keky to GND if you want to send it in upper case");
-  // Serial.println();  
 
   Bluefruit.begin();
   Bluefruit.setTxPower(4);    // Check bluefruit.h for supported values -40, -20, -16, -12, -8, -4, 0, 3, 4
@@ -164,28 +119,7 @@ void setup()
   // leds
   pinMode(connectedLed, OUTPUT);
 
-  // set up modifier key
-  //pinMode(shiftPin, INPUT_PULLUP);
-
-  /* Start BLE HID
-   * Note: Apple requires BLE device must have min connection interval >= 20m
-   * ( The smaller the connection interval the faster we could send data).
-   * However for HID and MIDI device, Apple could accept min connection interval 
-   * up to 11.25 ms. Therefore BLEHidAdafruit::begin() will try to set the min and max
-   * connection interval to 11.25  ms and 15 ms respectively for best performance.
-   */
   blehid.begin();
-
-  // Set callback for set LED from central
-  //blehid.setKeyboardLedCallback(set_keyboard_led);
-
-  /* Set connection interval (min, max) to your perferred value.
-   * Note: It is already set by BLEHidAdafruit::begin() to 11.25ms - 15ms
-   * min = 9*1.25=11.25 ms, max = 12*1.25= 15 ms 
-   */
-  /* Bluefruit.Periph.setConnInterval(9, 12); */
-
-  // Set up and start advertising
   startAdv();
 }
 
@@ -199,12 +133,6 @@ void loop()
   uint8_t modifier = 0;
   uint8_t count=0;
   uint8_t keycode[6] = { 0 };
-
-  // scan modifier key (only SHIFT), user implement ATL, CTRL, CMD if needed
-  // if ( 0 == digitalRead(shiftPin) )
-  // {
-  //   modifier |= KEYBOARD_MODIFIER_LEFTSHIFT;
-  // }
 
   // scan normal key and send report
   for(uint8_t i=0; i < pincount; i++)
